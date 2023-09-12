ef95a671c1d3cfa9daa2eb4ca84e027cdaa7bdc1
VitorLopes02 <vitinhoolopes.vl@gmail.com>
2023-09-12 18:20:36 -0300

    Alteração no index.

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <title>Consulta Receita WS</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light py-4">
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#" style="font-size: 24px;">Receita WS <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <h1>Digite o numero do CNPJ</h1>
+        <div class="mb-4">
+                        <!-- <label for="razaoSocial" class="form-label">Razão Social:</label> -->
+                        <input type="text" class="form-control" id="razaoSocial">
+                        <div class="mb-4">                        
+                        <a href="./validar.php" class="btn btn-primary">Validar</a>
+                    </div>
+        </div>
+    </main>
+
+    <footer></footer>
+    
+</body>
+</html>
