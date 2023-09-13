979058c1c47ee263b565d74be98be0d6b196198c
VitorLopes02 <vitinhoolopes.vl@gmail.com>
2023-09-13 13:55:50 -0300

    Consumindo a API com o front.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,7 +8,7 @@
     <title>Consulta Receita WS</title>
     <style>
         body {
-            background-color: #f0f0f0; /* Defina a cor de fundo do corpo */
+            background-color: #f0f0f0; 
         }
     </style>
 </head>
@@ -16,13 +16,12 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #c2c2ed; padding: 15px 0;">
         <div class="container">
-            <a class="navbar-brand" href="#" style="font-size: 20px; margin-right: 20px; font-weight: bold; font-family: 'Arial Rounded MT Bold', sans-serif;">Receita WS</a>
-            <!-- Adicione os links de navegação com a mesma fonte e estilos -->
+            <a class="navbar-brand" href="./index.php" style="font-size: 20px; margin-right: 20px; font-weight: bold; font-family: 'Arial Rounded MT Bold', sans-serif;">Receita WS</a>            
             <ul class="navbar-nav ml-auto" style="font-family: 'Arial Rounded MT Bold', sans-serif;">
                 <li class="nav-item">
                     <a class="nav-link" href="#" style="font-weight: bold; letter-spacing: 4px;">API</a>
                 </li>
-                <li class="nav-item" style="margin-left: 20px;"> <!-- Aumente o espaçamento aqui -->
+                <li class="nav-item" style="margin-left: 20px;"> 
                     <a class="nav-link" href="#" style="font-weight: bold; letter-spacing: 4px;">Verificar</a>
                 </li>
             </ul>
@@ -32,19 +31,20 @@
 
 <main class="d-flex justify-content-center align-items-center vh-100" style="margin-top: 160px;">
     <div class="p-4 rounded" style="background-color: #c2c2ed; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
-        <h1 class="mb-4" style="font-size: 20px; margin-right: 20px; font-weight: bold; font-family: 'Arial Rounded MT Bold', sans-serif; color: white;">Digite o número do CNPJ</h1>
-        <div class="mb-4">
-            <input type="text" class="form-control" id="razaoSocial">
-        </div>
-        <div class="mb-4">
-            <!-- Estilize o botão aqui -->
-            <a href="./validar.php" class="btn btn-outline-ligh rounded-pill" style="border-color: #d5d5f0; background-color: #d5d5f0; color: #6a6a6a; transition: background-color 0.3s;">
-                Gerar Documentos
-            </a>
-            <a href="./validar.php" class="btn btn-outline-ligh rounded-pill" style="border-color: #d5d5f0; background-color: #d5d5f0; color: #6a6a6a; transition: background-color 0.3s;">
-                Validar
-            </a>
-        </div>
+        <h1 class="mb-4" style="font-size: 20px; font-weight: bold; font-family: 'Arial Rounded MT Bold', sans-serif; color: white;">Digite o número do CNPJ</h1>
+        <form action="./validar.php" method="POST">
+            <div class="mb-4">
+                <input type="text" class="form-control" id="cnpj" name="cnpj">
+            </div>
+            <div class="mb-4">                
+                <button type="submit" class="btn btn-outline-light rounded-pill" style="border-color: #d5d5f0; background-color: #d5d5f0; color: #6a6a6a; transition: background-color 0.3s;">
+                    Gerar Documentos
+                </button>
+                <button type="submit" class="btn btn-outline-light rounded-pill" style="border-color: #d5d5f0; background-color: #d5d5f0; color: #6a6a6a; transition: background-color 0.3s;">
+                    Validar
+                </button>
+            </div>
+        </form>
     </div>
 </main>
     
--- validar.php
+++ validar.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,13 +8,13 @@
     <title>Consulta Receita WS</title>
     <style>
         body {
-            background-color: #f0f0f0; /* Defina a cor de fundo do corpo */
+            background-color: #f0f0f0; 
         }
         .main-container {
-            background-color: #c2c2ed; /* Cor de fundo da div principal */
-            padding: 20px; /* Espaçamento interno */
-            border-radius: 10px; /* Bordas arredondadas */
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2); /* Sombra */
+            background-color: #c2c2ed; 
+            padding: 20px; 
+            border-radius: 10px; 
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2); 
         }
     </style>
 </head>
@@ -23,13 +23,12 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #c2c2ed; padding: 15px 0;">
         <div class="container">
-            <a class="navbar-brand" href="#" style="font-size: 20px; margin-right: 20px; font-weight: bold; font-family: 'Arial Rounded MT Bold', sans-serif;">Receita WS</a>
-            <!-- Adicione os links de navegação com a mesma fonte e estilos -->
+            <a class="navbar-brand" href="./index.php" style="font-size: 20px; margin-right: 20px; font-weight: bold; font-family: 'Arial Rounded MT Bold', sans-serif;">Receita WS</a>            
             <ul class="navbar-nav ml-auto" style="font-family: 'Arial Rounded MT Bold', sans-serif;">
                 <li class="nav-item">
                     <a class="nav-link" href="#" style="font-weight: bold; letter-spacing: 4px;">API</a>
                 </li>
-                <li class="nav-item" style="margin-left: 20px;"> <!-- Aumente o espaçamento aqui -->
+                <li class="nav-item" style="margin-left: 20px;"> 
                     <a class="nav-link" href="#" style="font-weight: bold; letter-spacing: 4px;">Verificar</a>
                 </li>
             </ul>
@@ -41,57 +40,78 @@
     <div class="container main-container" style="margin-top: 100px;">
         <div class="row">
             <div class="col-md-6">
-                <form>
+                <form method="POST">
+                <?php
+                    require __DIR__.'/vendor/autoload.php';
+
+                    use \App\WebService\ReceitaWS;
+
+                    $obReceitaWS = new ReceitaWS;
+
+                    $resultado = array(
+                        'cnpj' => '',
+                        'nome' => '',
+                        'fantasia' => '',
+                        'status' => '',
+                        'logradouro' => '',
+                        'numero' => '',
+                        'cep' => '',
+                        'telefone' => '',
+                    );
+
+                    $cnpj = ''; 
+
+                    if(isset($_POST['cnpj'])) {
+                        $cnpj = $_POST['cnpj'];
+
+                        $consulta = $obReceitaWS->consultarCNPJ($cnpj);
+
+                        if(isset($consulta['error'])) {
+                            die($consulta['error']);
+                        }
+
+                        if(!empty($consulta)) {
+                            $resultado = $consulta;
+                        } else {
+                            echo '<div class="alert alert-danger" role="alert">CNPJ não encontrado.</div>';
+                        }
+                    }
+                ?>
+
                     <div class="mb-4">
                         <label for="cnpj" class="form-label font-weight-bold" style="color: white;">CNPJ:</label>
-                        <input type="text" class="form-control" id="cnpj" aria-describedby="cnpjHelp" value="60746948000112">
+                        <input type="text" class="form-control" id="cnpj" aria-describedby="cnpjHelp" name="cnpj" value="<?php echo $cnpj; ?>">
                     </div>
                     <div class="mb-4">
                         <label for="razaoSocial" class="form-label font-weight-bold">Razão Social:</label>
-                        <input type="text" class="form-control" id="razaoSocial" value="<?php echo $resultado['nome']; ?>">
+                        <input type="text" class="form-control" id="razaoSocial" name="nome" value="<?php echo $resultado['nome']; ?>">
                     </div>
                     <div class="mb-4">
                         <label for="nomeFantasia" class="form-label font-weight-bold">Nome Fantasia:</label>
-                        <input type="text" class="form-control" id="nomeFantasia" value="<?php echo $resultado['fantasia']; ?>">
+                        <input type="text" class="form-control" id="nomeFantasia" name="fantasia" value="<?php echo $resultado['fantasia']; ?>">
+                    </div>
+                    <div class="mb-4">
+                        <label for="statusEmpresa" class="form-label font-weight-bold">Status da Empresa:</label>
+                        <input type="text" class="form-control" id="statusEmpresa" name="status" value="<?php echo $resultado['status']; ?>">
                     </div>
                     <div class="mb-4">
                         <label for="logradouro" class="form-label font-weight-bold">Logradouro:</label>
-                        <input type="text" class="form-control" id="logradouro" value="<?php echo $resultado['logradouro']; ?>">
+                        <input type="text" class="form-control" id="logradouro" name="logradouro" value="<?php echo $resultado['logradouro']; ?>">
                     </div>
                 </form>
             </div>
             <div class="col-md-6">
-                <?php
-                require __DIR__.'/vendor/autoload.php';
-
-                use \App\WebService\ReceitaWS;
-
-                $obReceitaWS = new ReceitaWS;
-
-                $cnpj = '00000000000191';
-
-                $resultado = $obReceitaWS->consultarCNPJ($cnpj);
-
-                if(empty($resultado)) {
-                    die('Problemas ao consultar CNPJ');
-                }
-
-                if(isset($resultado['error'])) {
-                    die($resultado['error']);
-                }
-                ?>
-
                 <div class="mb-4">
                     <label for="numero" class="form-label font-weight-bold">Número:</label>
-                    <input type="text" class="form-control" id="numero" value="<?php echo $resultado['numero']; ?>">
+                    <input type="text" class="form-control" id="numero" name="numero" value="<?php echo $resultado['numero']; ?>">
                 </div>
                 <div class="mb-4">
                     <label for="cep" class="form-label font-weight-bold">CEP:</label>
-                    <input type="text" class="form-control" id="cep" value="<?php echo $resultado['cep']; ?>">
+                    <input type="text" class="form-control" id="cep" name="cep" value="<?php echo $resultado['cep']; ?>">
                 </div>
                 <div class="mb-4">
                     <label for="telefone" class="form-label font-weight-bold">Telefone:</label>
-                    <input type="tel" class="form-control" id="telefone" value="<?php echo $resultado['telefone']; ?>">
+                    <input type="tel" class="form-control" id="telefone" name="telefone" value="<?php echo $resultado['telefone']; ?>">
                 </div>
             </div>
         </div>
